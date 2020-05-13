4a4a91091a7d7a018f1345e67a5534c72813b11c
carolcarolcarol <35793842+carolcarolcarol@users.noreply.github.com>
2020-05-13 23:12:02 +0800

    Add files via upload

---
+++ leetcode/Reverse Integer/1.js
@@ -0,0 +1,9 @@
+var reverse = function(x) {
+    let now = Math.abs(x).toString().split("").reverse().join("");
+    if(x < 0){
+        return now <= Math.pow(2,31) ? -now : 0;
+    }else{
+        return now < Math.pow(2,31) ? now : 0;
+    }
+};
+
